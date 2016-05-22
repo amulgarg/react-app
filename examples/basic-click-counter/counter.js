a208d7637d9fbde08370e75d11d888ed22740bd5
Amul Garg <amul.garg@knolskape.com>
2016-05-22 23:01:59 +0530

    added todos react component

---
+++ examples/basic-click-counter/counter.js
@@ -0,0 +1,19 @@
+ (function(){
+   var Counter = React.createClass({
+    getInitialState: function () {
+      return { count: 0 };
+    },
+    handleClick: function () {
+      this.setState({
+        count: this.state.count + 1,
+      });
+    },
+    render: function () {
+      return (
+        <button onClick={this.handleClick}>
+          Click me! Number of clicks: {this.state.count}
+        </button>
+      );
+    }
+  });
+})();
